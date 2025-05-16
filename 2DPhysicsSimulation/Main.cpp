b98d52cd7c158c5fe39231263960be1ab7d917c7
nble92 <j.nble92@gmail.com>
2025-05-16 19:38:03 -0400

    Much better physics implementation. Still need proper scaling for values.

--- 2DPhysicsSimulation/Main.cpp
+++ 2DPhysicsSimulation/Main.cpp
@@ -10,104 +10,125 @@ enum screen {
 
 SDL_Event e;
 bool running = true;
-SDL_FRect r{ 10, 10, 100, 100 };
-SDL_FRect flr{ 0, SCREEN_HEIGHT - 4, SCREEN_WIDTH, 100 };
+
+
+class PhysicsObject
+
+{
+public:
+	SDL_FRect r{ 10, 10, 100, 100 };
+	float velocityX = 0.0f;
+	float velocityY = 0.0f;
+	float accelerationX = 0.0f;
+	float accelerationY = 0.0f;
+	float mass = 1.0f;
+	float restitution = 0.5f; // Bounciness
+	float friction = 0.5f; // Friction coefficient
+
+	void applyForce(float forceX, float forceY) {
+		accelerationX += forceX / mass;
+		accelerationY += forceY / mass;
+	}
+
+	void update(float deltaTime) {
+		// Apply acceleration to velocity
+		velocityX += accelerationX * deltaTime;
+		velocityY += accelerationY * deltaTime;
+
+		// Apply velocity to position
+		r.x += velocityX * deltaTime;
+		r.y += velocityY * deltaTime;
+
+		// Reset acceleration each frame
+		accelerationX = 0;
+		accelerationY = 0;
+	}
+
+	void handleFloorCollision(float floorY) {
+		if (r.y + r.h >= floorY) {
+			r.y = floorY - r.h;
+			velocityY *= -restitution; // bounce
+			velocityX *= friction;     // simple friction
+		}
+	}
+};
+
 
 
 
 namespace physics {
-	constexpr float GRAVITY = 9.8f;
-	constexpr float FRICTION = 0.5f;
-	constexpr float BOUNCE = 0.5f;
+	constexpr float GRAVITY = 0.025f; // meters/sec²
+	constexpr float PIXELS_PER_METER = 100.0f;
+
+	void applyGravity(PhysicsObject& obj) {
+		float gravityForce = obj.mass * GRAVITY;
+		obj.applyForce(0.0f, gravityForce * PIXELS_PER_METER);
+	}
+}
+
+SDL_FRect flr{ 0, SCREEN_HEIGHT - 4, SCREEN_WIDTH, 100 };
+PhysicsObject box;
+
+void update(PhysicsObject &obj,float deltaTime) {
+	// Update the physics object
+	
+	obj.update(deltaTime);
+	obj.handleFloorCollision(flr.y);
 }
 
 int main()
 
 {
+
+
+
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer("Hello Window", SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
 	
+	update(box, 0.016f);
 
-	while (running)
-	{
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_EVENT_QUIT) 
-			{
+	while (running) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_EVENT_QUIT) {
 				running = false;
 			}
-			else if (e.type == SDL_EVENT_KEY_DOWN)
-			{
-				switch (e.key.key)
-				{
+			else if (e.type == SDL_EVENT_KEY_DOWN) {
+				switch (e.key.key) {
 				case SDLK_RIGHT:
-					cout << "Right key pressed" << endl;
-					r.x += 50;
-
+					box.r.x += 50;
 					break;
-
 				case SDLK_LEFT:
-					cout << "Left key pressed" << endl;
-					r.x -= 50;
-
+					box.r.x -= 50;
 					break;
-
 				case SDLK_UP:
-					cout << "Up key pressed" << endl;
-					r.y -= 250;
+					box.r.y -= 250;
 					break;
 				case SDLK_DOWN:
-					cout << "Down key pressed" << endl;
-					r.y += 50;
+					box.r.y += 50;
 					break;
-
 				}
-
 			}
-
-			else if (e.type == SDL_EVENT_KEY_UP)
-			{
-				switch (e.key.key)
-				{
-				case SDLK_RIGHT:
-					cout << "Right key released" << endl;
-					break;
-
-				}
-
-			}
-			/*else if (e.type == SDL_EVENT_MOUSE_MOTION) {
-
-				SDL_GetMouseState(&r.x, &r.y);
-			}*/
-
-
-
 		}
-		
-			r.y += physics::GRAVITY * 0.1f; // Apply gravity to the rectangle's y position
 
-			//else 
-		if (r.y + r.h >= flr.y) // Check if the rectangle's bottom edge touches or passes the floor
-		{
-			r.y = flr.y - r.h; // Place the rectangle on top of the floor
-			// Apply friction or stop movement here
-		}
+		// Apply physics update
+		update(box, 0.016f);
+		// Apply gravity
+		physics::applyGravity(box);
 
 	
 		
 
-		cout << "Rectangle position: (" << r.x << ", " << r.y << ")" << endl;
+		//cout << "Rectangle position: (" << r.x << ", " << r.y << ")" << endl;
 		// Set the draw color to black for the background
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		// Clear the window
 		SDL_RenderClear(renderer);
 
 		SDL_SetRenderDrawColor(renderer, 220, 100, 40, 255);
-		SDL_RenderFillRect(renderer, &r);	
+		SDL_RenderFillRect(renderer, &box.r);	
 	
 		SDL_SetRenderDrawColor(renderer, 173, 216, 230, 255);		
 		SDL_RenderFillRect(renderer, &flr);
